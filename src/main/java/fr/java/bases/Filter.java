60d3de574abcb14ed75681a66d68f923b3187e38
Antoine BERBINEAU <antoine.berbineau.extia@axa.fr>
2022-10-25 10:51:12 +0200

    add implementation

--- src/main/java/fr/java/bases/Filter.java
+++ src/main/java/fr/java/bases/Filter.java
@@ -1,11 +1,21 @@
 package fr.java.bases;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Filter {
 
     public static <T> List<T> filter(List<T> originalLst, Predicate<T> predicate) {
-        throw new NotImplementedException("Please complete fr.java.bases.Filter#filter");
+        //throw new NotImplementedException("Please complete fr.java.bases.Filter#filter");
+        List<T> resultLst = new ArrayList<>();
+        
+        for(T item:originalLst) {
+            if(predicate.test(item)) {
+                resultLst.add(item);
+            }
+        }
+        
+        return resultLst;
     }
 }
--- src/main/java/fr/java/bases/Map.java
+++ src/main/java/fr/java/bases/Map.java
@@ -1,11 +1,20 @@
 package fr.java.bases;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 public class Map {
 
     public static <T,D> List<D> map(List<T> originalLst, Function<T,D> function) {
-        throw new NotImplementedException("Please complete fr.java.bases.Map#map");
+        //throw new NotImplementedException("Please complete fr.java.bases.Map#map");
+        
+        List<D> resultLst = new ArrayList<>();
+        
+        for(T item:originalLst) {
+            resultLst.add(function.apply(item));
+        }
+        
+        return resultLst;
     }
 }
