8d00a5bc5987e3d3912db02d98de47e5ce814d77
Antoine Berbineau <antoine.berbineau@gmail.com>
2022-11-03 16:44:01 +0100

    Add corrections for min and max

--- src/main/java/fr/java/bases/Comparison.java
+++ src/main/java/fr/java/bases/Comparison.java
@@ -7,10 +7,32 @@ import java.util.Optional;
 public class Comparison {
     
     public static <T> Optional<T> min(List<T> originalLst, Comparator<? super T> comparator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Comparison#min");
+        // throw new NotImplementedException("Please complete fr.java.bases.Comparison#min");
+        if (originalLst.isEmpty()) {
+            return Optional.empty();
+        }
+        
+        T minimum = originalLst.get(0);
+        for (T item : originalLst) {
+            if (comparator.compare(minimum, item) > 0) {
+                minimum = item;
+            }
+        }
+        return Optional.ofNullable(minimum);
     }
     
     public static <T> Optional<T> max(List<T> originalLst, Comparator<? super T> comparator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Comparison#max");
+        // throw new NotImplementedException("Please complete fr.java.bases.Comparison#max");
+        if (originalLst.isEmpty()) {
+            return Optional.empty();
+        }
+        
+        T minimum = originalLst.get(0);
+        for (T item : originalLst) {
+            if (comparator.compare(minimum, item) < 0) {
+                minimum = item;
+            }
+        }
+        return Optional.ofNullable(minimum);
     }
 }
