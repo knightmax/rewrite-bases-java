234500b078a2168c3dbafc04031441f4ef6904fb
Antoine Berbineau <antoine.berbineau@gmail.com>
2022-11-03 15:59:15 +0100

    Add flatmap correction

--- src/main/java/fr/java/bases/FlatMap.java
+++ src/main/java/fr/java/bases/FlatMap.java
@@ -1,5 +1,6 @@
 package fr.java.bases;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -7,6 +8,11 @@ import java.util.stream.Stream;
 public class FlatMap {
 
     public static <T,D> List<D> flatMap(List<List<T>> originalLst, Function<List<T>, Stream<? extends D>> function) {
-        throw new NotImplementedException("Please complete fr.java.bases.FlatMap#flatMap");
+        //throw new NotImplementedException("Please complete fr.java.bases.FlatMap#flatMap");
+        List<D> result = new ArrayList<>();
+        for(List<T> subList:originalLst) {
+            result.addAll(function.apply(subList).toList());
+        }
+        return result;
     }
 }
