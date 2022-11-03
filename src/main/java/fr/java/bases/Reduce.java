1d3ddfa3aad4eb7d94fdedf6e3cb9d480b113fb0
Antoine Berbineau <antoine.berbineau@gmail.com>
2022-11-03 16:48:27 +0100

    Add correction for reduce

--- src/main/java/fr/java/bases/Reduce.java
+++ src/main/java/fr/java/bases/Reduce.java
@@ -7,7 +7,22 @@ import java.util.function.BinaryOperator;
 public class Reduce {
     
     public static <T> Optional<T> reduce(List<T> originalLst, BinaryOperator<T> accumulator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Reduce#reduce");
+        // throw new NotImplementedException("Please complete fr.java.bases.Reduce#reduce");
+        if (originalLst.isEmpty()) {
+            return Optional.empty();
+        }
+        
+        T reduction = originalLst.get(0);
+        
+        if (1 == originalLst.size()) {
+            return Optional.ofNullable(reduction);
+        }
+        
+        for (int i = 1; i < originalLst.size(); i++) {
+            reduction = accumulator.apply(reduction, originalLst.get(i));
+        }
+        
+        return Optional.ofNullable(reduction);
     }
     
 }
