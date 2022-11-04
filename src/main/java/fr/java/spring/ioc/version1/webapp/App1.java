363a40f30faaa9df9cabecf9a7de48afa29fa410
Antoine Berbineau <antoine.berbineau@gmail.com>
2022-11-04 11:03:47 +0100

    Add correction for Summer 1.0

--- src/main/java/fr/java/spring/ioc/version1/webapp/App1.java
+++ src/main/java/fr/java/spring/ioc/version1/webapp/App1.java
@@ -3,16 +3,18 @@ package fr.java.spring.ioc.version1.webapp;
 import fr.java.spring.ioc.common.model.DataGenerator;
 import fr.java.spring.ioc.common.model.Person;
 import fr.java.spring.ioc.version1.webapp.dao.PersonDAO;
+import fr.java.spring.ioc.version1.webapp.dao.PersonDAOImpl;
 import fr.java.spring.ioc.version1.webapp.service.PersonService;
+import fr.java.spring.ioc.version1.webapp.service.PersonServiceImpl;
 
 import java.util.UUID;
 
 public class App1 {
-
+    
     public void exec() {
-        final PersonDAO personDAO = null; // TODO Instantiate PersoDAO
-        final PersonService personService = null; // TODO Instantiate PersonService
-
+        final PersonDAO personDao = new PersonDAOImpl();
+        final PersonService personService = new PersonServiceImpl(personDao);
+        
         Person person = DataGenerator.createPerson();
         UUID saved = personService.save(person);
         personService.findById(saved);
