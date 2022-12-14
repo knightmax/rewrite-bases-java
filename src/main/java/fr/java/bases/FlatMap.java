1da5620fd1b6aa7430c7b1342af8c2b23a5b3af6
Guillaume Dufrêne <guillaume.dufrene@axa.fr>
2022-12-14 14:00:27 +0100

    Review flatMap to handle List rather than Stream, function is an Item to List handler

--- src/main/java/fr/java/bases/FlatMap.java
+++ src/main/java/fr/java/bases/FlatMap.java
@@ -3,17 +3,12 @@ package fr.java.bases;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class FlatMap {
 
-    public static <T,D> List<D> flatMap(List<List<T>> originalLst, Function<List<T>, Stream<? extends D>> function) {
-        List<D> calculated = new ArrayList<>();
-        for (List<T> list : originalLst) {
-            try (Stream<? extends D> result = function.apply(list)) {
-                calculated.addAll(result.toList());
-            }
-        }
-        return calculated;
+    public static <T,D> List<D> flatMap(List<T> originalLst, Function<T, List<D>> function) {
+        // TODO: implement this method
+       throw new NotImplementedException("Please complete fr.java.bases.FlatMap#flatMap");
     }
+
 }
