e911f0e901e9fe0d411727918effc7fc690f05f5
CHEVALIER Maximilien <a853jz@login.axa>
2022-12-15 09:18:36 +0100

    Update flatmap correction

--- src/main/java/fr/java/bases/FlatMap.java
+++ src/main/java/fr/java/bases/FlatMap.java
@@ -1,14 +1,16 @@
 package fr.java.bases;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
 
 public class FlatMap {
 
     public static <T,D> List<D> flatMap(List<T> originalLst, Function<T, List<D>> function) {
-        // TODO: implement this method
-       throw new NotImplementedException("Please complete fr.java.bases.FlatMap#flatMap");
+        List<D> calculated = new LinkedList<>();
+        for (T element : originalLst) {
+            calculated.addAll(function.apply(element));
+        }
+        return calculated;
     }
-
 }
